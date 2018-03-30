2c5d1326392fe90a5606353b69b45ced8fb4f7ea
Viktor Hansen <viktor.hansen@coolcompany.se>
2018-03-30 21:47:28 +0200

    Initial commit. Contains functionality for loading a file of words and a naive solution for finding candidate words based on user input.

---
+++ Library.fs
@@ -0,0 +1,5 @@
+namespace HyperWord
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ PredictiveEngine.fs
@@ -0,0 +1,18 @@
+﻿module HyperWord.PredictiveEngine
+
+open System.IO
+
+let pathToDict = Path.Combine(__SOURCE_DIRECTORY__, "dict.txt")
+
+let loadDictFromPath path = 
+    File.ReadAllLines path
+
+let loadDict () = 
+    let dict = loadDictFromPath pathToDict
+    dict
+
+let sweDict = loadDict()
+
+let autocomplete (prefix:string) (dict:string[]) = 
+    let candidates = dict |> Array.filter (fun word -> word.StartsWith(prefix))
+    candidates
